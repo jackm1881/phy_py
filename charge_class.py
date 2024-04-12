80bcbd45f2551229b35642779990731952cf45f8
jackm1881 <jackm1881@gmail.com>
2024-04-12 14:01:28 -0400

    modified for user input

--- charge_class.py
+++ charge_class.py
@@ -1,10 +1,9 @@
 class ChargedParticle:
-    def __init__(self,chrg: float,pos: list) -> None:
+    def __init__(self, chrg: float, pos: list) -> None:
         self.charge = chrg
-        while len(pos) != 3:
-            pos.append(0.0)
-        self.pos = pos
-        
-
+        if len(pos) != 3:
+            raise ValueError("Position must have three components.")
+        self.pos = pos  
     def __repr__(self) -> str:
-        r = "charge: {} C, x = {}, y = {}, z = {}".format(self.charge,self.pos[0],self.pos[1],self.pos[2])
+        r = "Charge: {} C. Positon: x = {} m, y = {} m, z = {} m".format(self.charge,self.pos[0],self.pos[1],self.pos[2])
+        return r
